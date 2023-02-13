2b2fe2833e0a7a1bd014fee98ef7473bed7867bc
Muhammad imran <manig11439@gmail.com>
2023-02-13 20:49:05 +0500

    forgot password

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    use HasFactory;
+    protected $fillable = ['username', 'mobile', 'first_name', 'last_name', 'email'];
+    protected $guarded = ['password'];
 }
